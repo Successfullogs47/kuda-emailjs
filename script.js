50f7bdde6c9cf2d22ad55c2fda064ee1be513d78
Successfullogs47 <168442633+Successfullogs47@users.noreply.github.com>
2024-05-04 14:34:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ document.getElementById("login").onsubmit = function (event) {
     }
   );
 };
+
+
+const eye = document.querySelector(".formInput i.fa-eye");
+
+eye.onclick = () => {
+  if (password.type === "password") {
+    password.type = "text";
+    eye.classList.replace("fa-eye", "fa-eye-slash");
+  } else {
+    password.type = "password";
+    eye.classList.replace("fa-eye-slash", "fa-eye");
+  }
+};
